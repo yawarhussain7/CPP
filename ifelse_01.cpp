7ce509b18ac4e4f1a7ee6bb9367c0ef1022b0f49
Yawar Hussain <yawarhussain793@gmail.com>
2023-10-07 00:20:30 +0530

    IF Else Program

---
+++ ifelse_01.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int saving ;
+    cout<<"Enter your saving : ";
+    cin>>saving;
+
+    if(saving > 5000)
+    {
+        cout<<"Neha "<<endl;
+    }
+    else
+    cout<<"Rashmi"<<endl;
+
+    return 0;
+}
