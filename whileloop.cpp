6959b7401033972894e3d7c611905de16afd4190
Yawar Hussain <yawarhussain793@gmail.com>
2023-10-07 12:37:24 +0530

    While loop in C++

---
+++ whileloop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num = 1;
+  
+
+    while(num > 0)
+    {
+        cout<<"Enter a number : ";
+        cin>>num;
+        cout<<num<<endl;
+    }
+
+}
